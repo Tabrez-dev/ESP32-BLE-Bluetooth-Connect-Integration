c78e5d6618e13851aea9c36271b05f683db24a98
Tabrez Ahmed <75200693+Tabrez-dev@users.noreply.github.com>
2024-11-07 15:52:59 +0530

    Create bt_ble.c
    Code for the integration of ble and bt. functions can be used to intialise bt and ble in other code bases

---
+++ bt_ble.c
@@ -0,0 +1,477 @@
+#include <string.h>
+
+#include "esp_log.h"
+#include "esp_bt.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "vrc_wifi_bt_ble.h"
+#include "a2dp_stream.h"
+#include "bluetooth_service.h"
+#include "esp_peripherals.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+#include "cJSON.h"
+
+#define GATTS_TAG "GATTS_INIT"
+#define PROFILE_A_APP_ID 0
+
+extern audio_board_handle_t board_handle;
+audio_element_handle_t bt_stream_reader;
+static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+#define CHAR_DECLARATION_SIZE (sizeof(uint8_t))
+int ir_devid ;
+static const uint16_t GATTS_SERVICE_UUID_TEST_A =  0x00FF;
+static const uint16_t  GATTS_CHAR1_UUID_TEST_A  =  0xFF01;
+static const uint16_t GATTS_CHAR2_UUID_TEST_A   =  0xFF02;
+static const uint16_t GATTS_DESCR_UUID_TEST_A   =  0xFF21;
+
+static prepare_type_env_t a_prepare_write_env;
+static const uint16_t descriptor_declaration_uuid = ESP_GATT_UUID_CHAR_DESCRIPTION;
+
+static const uint16_t primary_service_uuid         = ESP_GATT_UUID_PRI_SERVICE;
+
+
+static const uint16_t character_declaration_uuid   = ESP_GATT_UUID_CHAR_DECLARE;
+// Properties for Characteristic 1 (Write)
+static esp_gatt_char_prop_t char1_properties = ESP_GATT_CHAR_PROP_BIT_WRITE;
+
+// Properties for Characteristic 2 (Notify)
+static esp_gatt_char_prop_t char2_properties = ESP_GATT_CHAR_PROP_BIT_NOTIFY;
+#define TEST_DEVICE_NAME            "BLE_TEST"
+void handle_test_event(const char *message);
+
+
+static uint16_t service1_handle;
+static uint16_t char1_write_handle;
+uint16_t char2_notify_handle;
+static uint16_t char2_notify_desc_handle_A;
+
+uint16_t gatt_if;
+uint16_t conn_id;
+#define TEST_MANUFACTURER_DATA_LEN  17
+#define GATTS_DEMO_CHAR_VAL_LEN_MAX 0xFF
+#define PREPARE_BUF_MAX_SIZE 1024
+uint16_t char1_str[] = {0x1111,0x2222,0x3333};
+esp_gatt_char_prop_t a_property = 0;
+esp_gatt_char_prop_t b_property = 0;
+
+static esp_attr_value_t gatts_demo_char1_val =
+{
+    .attr_max_len = GATTS_DEMO_CHAR_VAL_LEN_MAX,
+    .attr_len     = sizeof(char1_str),
+    .attr_value   = char1_str,
+};
+
+static uint8_t adv_config_done = 0;
+#define adv_config_flag      (1 << 0)
+#define scan_rsp_config_flag (1 << 1)
+
+#ifdef CONFIG_SET_RAW_ADV_DATA
+static uint8_t raw_adv_data[] = {
+        0x02, 0x01, 0x06,
+        0x02, 0x0a, 0xeb, 0x03, 0x03, 0xab, 0xcd
+};
+static uint8_t raw_scan_rsp_data[] = {
+        0x0f, 0x09, 0x45, 0x53, 0x50, 0x5f, 0x47, 0x41, 0x54, 0x54, 0x53, 0x5f, 0x44,
+        0x45, 0x4d, 0x4f
+};
+#else
+
+static uint8_t adv_service_uuid128[32] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xEE, 0x00, 0x00, 0x00,
+    //second uuid, 32bit, [12], [13], [14], [15] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+};
+
+// The length of adv data must be less than 31 bytes
+//static uint8_t test_manufacturer[TEST_MANUFACTURER_DATA_LEN] =  {0x12, 0x23, 0x45, 0x56};
+//adv data
+static esp_ble_adv_data_t adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = false,
+    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
+    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+// scan response data
+static esp_ble_adv_data_t scan_rsp_data = {
+    .set_scan_rsp = true,
+    .include_name = true,
+    .include_txpower = true,
+    //.min_interval = 0x0006,
+    //.max_interval = 0x0010,
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+#endif /* CONFIG_SET_RAW_ADV_DATA */
+
+static esp_ble_adv_params_t adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x40,
+    .adv_type           = ADV_TYPE_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    //.peer_addr            =
+    //.peer_addr_type       =
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+
+#define PROFILE_NUM 1
+
+
+struct gatts_profile_inst {
+    esp_gatts_cb_t gatts_cb;
+    uint16_t gatts_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_handle;
+    esp_gatt_srvc_id_t service_id;
+    uint16_t char_handle;
+    esp_bt_uuid_t char_uuid;
+    esp_gatt_perm_t perm;
+    esp_gatt_char_prop_t property;
+    uint16_t descr_handle;
+    esp_bt_uuid_t descr_uuid;
+};
+
+/* One gatt-based profile one app_id and one gatts_if, this array will store the gatts_if returned by ESP_GATTS_REG_EVT */
+static struct gatts_profile_inst gl_profile_tab[PROFILE_NUM] = {
+    [PROFILE_A_APP_ID] = {
+        .gatts_cb = gatts_profile_a_event_handler,
+        .gatts_if = ESP_GATT_IF_NONE,       /* Not get the gatt_if, so initial is ESP_GATT_IF_NONE */
+    }
+};
+
+
+static const esp_gatts_attr_db_t gatt_db_a[] = {
+    // Service A Declaration
+    [0] = {
+        {ESP_GATT_AUTO_RSP},
+        {ESP_UUID_LEN_16, (uint8_t *)&primary_service_uuid, ESP_GATT_PERM_READ,
+         sizeof(uint16_t), sizeof(GATTS_SERVICE_UUID_TEST_A), (uint8_t *)&GATTS_SERVICE_UUID_TEST_A}
+    },//number of bits in uuid, address of service /char, permissions, uuid size, uuid, pointer to uuid
+
+    // Characteristic 1 Declaration for Service A (Write)
+    [1] = {
+        {ESP_GATT_AUTO_RSP},
+        {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+         CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char1_properties}
+    },//last one is properties of characteristic, prpoerty is different from permission
+
+    // Characteristic 1 Value for Service A
+    [2] = {
+        {ESP_GATT_AUTO_RSP},
+        {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR1_UUID_TEST_A, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+         GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(gatts_demo_char1_val), (uint16_t *)&gatts_demo_char1_val}
+    },//give uuid of charcteristic
+
+    // Characteristic 2 Declaration for Service A (Notify)
+    [3] = {
+        {ESP_GATT_AUTO_RSP},
+        {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
+         CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char2_properties}
+    },
+
+    // Characteristic 2 Value for Service A
+    [4] = {
+        {ESP_GATT_AUTO_RSP},
+        {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR2_UUID_TEST_A, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+         GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(gatts_demo_char1_val), (uint8_t *)&gatts_demo_char1_val}
+    },
+
+    // Descriptor for Characteristic 2 (Notify) in Service A
+    [5] = {
+        {ESP_GATT_RSP_BY_APP},
+        {ESP_UUID_LEN_16, (uint8_t *)&descriptor_declaration_uuid, ESP_GATT_PERM_READ,
+         sizeof(uint16_t), sizeof(GATTS_DESCR_UUID_TEST_A), (uint8_t *)&GATTS_DESCR_UUID_TEST_A}
+    },
+};
+
+    
+
+static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param) {
+    switch (event) {
+        case ESP_GATTS_REG_EVT:
+            // Simple device name set
+
+            if (set_dev_name_ret) {
+                ESP_LOGE("GATTS", "Set device name failed, error code = %x", set_dev_name_ret);
+            }
+            break;
+
+        case ESP_GATTS_READ_EVT: {
+            ESP_LOGI("GATTS", "Characteristic read, conn_id %d, handle %d", param->read.conn_id, param->read.handle);
+
+            esp_gatt_rsp_t rsp;
+            memset(&rsp, 0, sizeof(esp_gatt_rsp_t));
+            rsp.attr_value.handle = param->read.handle;
+            rsp.attr_value.len = 4; // Send 4 bytes of data
+            rsp.attr_value.value[0] = 0xde;
+            rsp.attr_value.value[1] = 0xed;
+            rsp.attr_value.value[2] = 0xbe;
+            rsp.attr_value.value[3] = 0xef;
+
+            esp_err_t ret = esp_ble_gatts_send_response(gatts_if, param->read.conn_id, param->read.trans_id, ESP_GATT_OK, &rsp);
+            if (ret == ESP_OK) {
+                ESP_LOGI("GATTS", "Response sent successfully");
+            } else {
+                ESP_LOGE("GATTS", "Failed to send response, error code: 0x%x", ret);
+            }
+            break;
+        }
+
+        case ESP_GATTS_WRITE_EVT: {
+            ESP_LOGI("GATTS", "GATT_WRITE_EVT, conn_id %d, handle %d", param->write.conn_id, param->write.handle);
+            if (!param->write.is_prep) {
+                uint8_t received_value = param->write.value[0]; // Get the first byte
+                ESP_LOGI("GATTS", "Received write value: %d", received_value);
+                // Handle the write logic based on the received value (simplified for now)
+            }
+            break;
+        }
+
+        case ESP_GATTS_DISCONNECT_EVT:
+            ESP_LOGI("GATTS", "Disconnected, reason 0x%x", param->disconnect.reason);
+            // Restart advertising after disconnect
+            esp_ble_gap_start_advertising(&adv_params);
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+    switch (event) {
+#ifdef CONFIG_SET_RAW_ADV_DATA
+    case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:
+        adv_config_done &= (~adv_config_flag);
+        if (adv_config_done==0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    case ESP_GAP_BLE_SCAN_RSP_DATA_RAW_SET_COMPLETE_EVT:
+        adv_config_done &= (~scan_rsp_config_flag);
+        if (adv_config_done==0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+#else
+    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+        adv_config_done &= (~adv_config_flag);
+        if (adv_config_done == 0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    case ESP_GAP_BLE_SCAN_RSP_DATA_SET_COMPLETE_EVT:
+        adv_config_done &= (~scan_rsp_config_flag);
+        if (adv_config_done == 0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+#endif
+    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+        //advertising start complete event to indicate advertising start successfully or failed
+        if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(GATTS_TAG, "Advertising start failed\n");
+        }
+        break;
+    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+        if (param->adv_stop_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(GATTS_TAG, "Advertising stop failed\n");
+        } else {
+            ESP_LOGI(GATTS_TAG, "Stop adv successfully\n");
+        }
+        break;
+    case ESP_GAP_BLE_UPDATE_CONN_PARAMS_EVT:
+         ESP_LOGI(GATTS_TAG, "update connection params status = %d, min_int = %d, max_int = %d,conn_int = %d,latency = %d, timeout = %d",
+                  param->update_conn_params.status,
+                  param->update_conn_params.min_int,
+                  param->update_conn_params.max_int,
+                  param->update_conn_params.conn_int,
+                  param->update_conn_params.latency,
+                  param->update_conn_params.timeout);
+        break;
+    default:
+        break;
+    }
+}
+
+void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param)
+{
+    /* If event is register event, store the gatts_if for each profile */
+    if (event == ESP_GATTS_REG_EVT) {
+        if (param->reg.status == ESP_GATT_OK) {
+            gl_profile_tab[param->reg.app_id].gatts_if = gatts_if;
+            gatt_if = gatts_if;
+            ESP_LOGE("gatt","gatt_if data is:%d\n",gatt_if);
+        } else {
+            ESP_LOGI(GATTS_TAG, "Reg app failed, app_id %04x, status %d\n",
+                    param->reg.app_id,
+                    param->reg.status);
+            return;
+        }
+    }
+
+    /* If the gatts_if equal to profile A, call profile A cb handler,
+     * so here call each profile's callback */
+    do {
+        int idx;
+        for (idx = 0; idx < PROFILE_NUM; idx++) {
+            if (gatts_if == ESP_GATT_IF_NONE || /* ESP_GATT_IF_NONE, not specify a certain gatt_if, need to call every profile cb function */
+                    gatts_if == gl_profile_tab[idx].gatts_if) {
+                if (gl_profile_tab[idx].gatts_cb) {
+                    gl_profile_tab[idx].gatts_cb(event, gatts_if, param);
+                }
+            }
+        }
+    } while (0);
+}
+
+
+// BLE Initialization
+void ble_initialization(void)
+{
+    esp_err_t ret;
+
+    // Release unused BT modes
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ret = esp_bt_controller_init(&bt_cfg);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s initialize controller failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    ret = esp_bt_controller_enable(ESP_BT_MODE_BTDM);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s enable controller failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+    ret = esp_bluedroid_init();
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s init bluetooth failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+    ret = esp_bluedroid_enable();
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s enable bluetooth failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+    ret = esp_ble_gatts_register_callback(gatts_event_handler);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "gatts register error, error code = %x", ret);
+        return;
+    }
+    ret = esp_ble_gap_register_callback(gap_event_handler);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "gap register error, error code = %x", ret);
+        return;
+    }
+    ret = esp_ble_gatts_app_register(PROFILE_A_APP_ID);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "gatts app register error, error code = %x", ret);
+        return;
+    }
+    esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(517);
+    if (local_mtu_ret) {
+        ESP_LOGE(GATTS_TAG, "set local MTU failed, error code = %x", local_mtu_ret);
+    }
+}
+
+static const char *TAG = "BT_TEST";
+
+// A2DP Sink Callback for Bluetooth Classic
+static void user_a2dp_sink_cb(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *param)
+{
+    ESP_LOGI(TAG, "A2DP sink user cb");
+    switch (event) {
+        case ESP_A2D_CONNECTION_STATE_EVT:
+            if (param->conn_stat.state == ESP_A2D_CONNECTION_STATE_DISCONNECTED) {
+                ESP_LOGW(TAG, "A2DP disconnected");
+            } else if (param->conn_stat.state == ESP_A2D_CONNECTION_STATE_CONNECTED) {
+                ESP_LOGI(TAG, "A2DP connected");
+            }
+            break;
+        case ESP_A2D_AUDIO_STATE_EVT:
+            if (param->audio_stat.state == ESP_A2D_AUDIO_STATE_REMOTE_SUSPEND) {
+                ESP_LOGW(TAG, "Audio stopped");
+            } else if (param->audio_stat.state == ESP_A2D_AUDIO_STATE_STARTED) {
+                ESP_LOGW(TAG, "Audio started");
+            }
+            break;
+        default:
+            ESP_LOGI(TAG, "User cb A2DP event: %d", event);
+            break;
+    }
+}
+
+// Initialize Bluetooth Classic (without affecting BLE)
+void init_bt_test(void)
+{
+    esp_err_t ret;
+    esp_periph_config_t periph_cfg = DEFAULT_ESP_PERIPH_SET_CONFIG();
+    esp_periph_set_handle_t set = esp_periph_set_init(&periph_cfg);
+    esp_err_t set_dev_name_ret = esp_bt_dev_set_device_name("BT_TEST");
+    if (set_dev_name_ret) {
+        ESP_LOGE(TAG, "Set BT device name failed, error code = %x, line(%d)", set_dev_name_ret, __LINE__);
+    }
+    ESP_LOGI(TAG, "[4.3] Create Bluetooth peripheral");
+    esp_periph_handle_t bt_periph = bt_create_periph();
+
+    ESP_LOGI(TAG, "[4.4] Start peripherals");
+    esp_periph_start(set, bt_periph);
+
+#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0))
+    esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+#else
+    esp_bt_gap_set_scan_mode(ESP_BT_SCAN_MODE_CONNECTABLE_DISCOVERABLE);
+#endif
+
+    a2dp_stream_config_t a2dp_config = {
+        .type = AUDIO_STREAM_READER,
+        .user_callback = {user_a2dp_sink_cb},
+#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0))
+        .audio_hal = board_handle->audio_hal,
+#endif
+    };
+    bt_stream_reader = a2dp_stream_init(&a2dp_config);
+}
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Initializing BLE and Bluetooth Classic");
+
+    // Initialize BLE functionality
+    ble_initialization();
+
+    // Initialize Bluetooth Classic with A2DP support
+    init_bt_test();
+
+    ESP_LOGI(TAG, "Initialization complete");
+}
